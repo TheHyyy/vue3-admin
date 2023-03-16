e311845455514ccad45a0db873a3cb190a31663b
侯雨 <houyu@le.com>
2023-03-16 15:53:12 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <el-button>Default</el-button>
+  <el-button>Default</el-button>
+  <el-button size="small" type="primary">
+    <span>更多</span>
+    <el-icon class="el-icon--right"><arrow-down /></el-icon>
+  </el-button>
+  <HelloWorld msg="Vite + Vue3" />
+  <div>222</div>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+// UI框架 element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 引入暗黑模式 element-plus 2.2 内置暗黑模式
+import 'element-plus/theme-chalk/dark/css-vars.css'
+// 自定义暗黑模式
+import "@/styles/element-dark.scss";
+// 引入阿里图标库
+// import "@/assets/iconfont/iconfont.css";
+// import "@/assets/iconfont/iconfont.js";
+
+
+const app = createApp(App)
+app.use(ElementPlus).mount('#app')
